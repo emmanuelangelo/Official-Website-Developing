e65cef1cf48355f371bfa8ba0093bfe49c6a24d6
Petar <mihaljevic.petar@gmail.com>
2022-02-05 17:16:33 +0100

    Initialization

---
+++ website/src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
