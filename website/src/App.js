d16a4c615e1e97e1d171fd3312846ec454719a6a
Petar <mihaljevic.petar@gmail.com>
2022-02-05 17:30:04 +0100

    Initial commit

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import Header from './components/Header/Header';
+import Body from './components/Body/Body';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
